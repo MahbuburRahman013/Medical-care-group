7557fcde092c58f39b8b2ab7eea69163f3e135e8
mspsohan <mspsohan@gmail.com>
2024-01-03 16:22:00 +0600

    Added some folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
- 
-
-  return (
-    <>
-      
-       <h1>hello world</h1>
-    </>
-  )
+	return (
+		<>
+			<h2>Hello Bangladesh</h2>
+			<h1>hello world</h1>
+		</>
+	);
 }
 
-export default App
+export default App;
